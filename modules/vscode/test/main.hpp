d98717fe7f206c3ed9af441b72c56bb12f489e0b
Pixailz <pixailz1@gmail.com>
2023-01-29 22:15:11 +0100

    :package: vscode - add snipets

---
+++ modules/vscode/test/main.hpp
@@ -0,0 +1,42 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.hpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: brda-sil <brda-sil@students.42angouleme    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/01/29 19:39:20 by brda-sil          #+#    #+#             */
+/*   Updated: 2023/01/29 20:42:58 by brda-sil         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+/*#BEGIN#_____________________>#_|DEPENDENCIES|_#<__________________________##*/
+
+#include <not_a_library>
+/****************
+ * <object>		object
+ * <function>	function()
+ */
+
+/*#END#_______________________<#_|DEPENDENCIES|_#>__________________________##*/
+
+#ifndef _CLASS
+# define _CLASS
+
+class
+{
+	private:
+		// VAR(S)
+
+
+		// INTERNAL FUNCTION(S)
+
+	public:
+		// CONSTRUCTOR(S)
+		::(void);
+
+		// DESTRUCTOR(S)
+		::(void);
+};
+
+# endif // _CLASS
---
+++ modules/vscode/test/run.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+#coding:utf-8
+
+#
+# HEADER
+
+ #
+#
+
+def main():
+	pass
+
+main()
