607a71f0b3b7e0b6a26c26a2328427f498ed84c9
Pixailz <Pixailz1@gmail.com>
2023-10-09 19:11:10 +0200

    gtk config

--- modules/i3/conf/polybar/launch.sh
+++ modules/i3/conf/polybar/launch.sh
@@ -1,13 +1,28 @@
-#!/bin/sh
+#!/usr/bin/env bash
 
-killall -q polybar
+# Terminate already running bar instances
+# If all your bars have ipc enabled, you can use
+polybar-msg cmd quit
+# Otherwise you can use the nuclear option:
+# killall -q polybar
 
-while pgrep -u ${UID} -x polybar >/dev/null; do
-	sleep 1
-done
+# Launch bar1 and bar2
+echo "---" | tee -a /tmp/polybar1.log #/tmp/polybar2.log
+polybar bar1 2>&1 | tee -a /tmp/polybar1.log & disown
+# polybar bar2 2>&1 | tee -a /tmp/polybar2.log & disown
 
-polybar top &
+echo "Bars launched..."
 
-# if [ $(xrandr -q | grep 'HDMI connected') ]; then
-# 	polybar top_external &
-# fi
+# #!/bin/sh
+
+# killall -q polybar
+
+# while pgrep -u ${UID} -x polybar >/dev/null; do
+# 	sleep 1
+# done
+
+# polybar top &
+
+# # if [ $(xrandr -q | grep 'HDMI connected') ]; then
+# # 	polybar top_external &
+# # fi
