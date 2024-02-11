b8636d1c7c05c21f26f92a53e72af4701f6db9ff
Pixailz <Pixailz1@gmail.com>
2024-02-11 14:59:07 +0100

    i3: polybar: config for multiple screen

--- modules/i3/conf/polybar/launch.sh
+++ modules/i3/conf/polybar/launch.sh
@@ -11,24 +11,25 @@ echo "---" | tee -a /tmp/${SELECTED_BAR[@]}
 launch_poly_bar()
 {
 	local	bar_name="${1?}"
-	local	monitor_index="${2:-1}"
+	local	monitor_index="${2?}"
 	local	bar_monitor
 
-	bar_monitor="$(xrandr --query | grep "${monitor_index} connected" | cut -d' ' -f1)"
+	bar_monitor="$(xrandr --query | grep "${monitor_index}.* connected" | cut -d' ' -f1)"
 	BAR_MONITOR=${bar_monitor} \
 		polybar "${bar_name}" 2>&1 | \
 			tee -a "/tmp/${bar_name}.log" & \
 				disown
 }
+
 NB_MONITOR="$(xrandr --query | grep -E "[0-9]+ connected" | wc -l)"
 
 case ${NB_MONITOR:-1} in
 	1)
-		launch_poly_bar "primary_top_full"
+		launch_poly_bar "primary_top_full" "DP"
 	;;
 	2)
-		launch_poly_bar "primary_top_right"
-		launch_poly_bar "secondary_top_left" 2
+		launch_poly_bar "primary_top_right" "HDMI"
+		launch_poly_bar "secondary_top_left" "DP"
 	;;
 	*) notify-send "[POLYBAR]: found ${NB_MONITOR}, but no layout associated";
 esac
